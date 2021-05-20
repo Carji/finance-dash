8c2aa78eb90f278461cdd6e74af1ba1582fb21f9
Carji <51406858+Carji@users.noreply.github.com>
2021-05-20 17:39:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -76,9 +76,9 @@ elif analysis == "SP500":
     st.markdown(string_logo2, unsafe_allow_html=True)
 
     st.header('**Representación sencilla del valor seleccionado**')
-    tickercopyDf=tickerDf
-    tickercopyDf=tickercopyDf.drop(['Volume', 'Dividends','Stock Splits'], axis=1)
-    st.line_chart(tickercopyDf,use_container_width=True)
+    tickercopyDf2=tickerDf2
+    tickercopyDf2=tickercopyDf2.drop(['Volume', 'Dividends','Stock Splits'], axis=1)
+    st.line_chart(tickercopyDf2,use_container_width=True)
 
     st.header('**Tabla/DF del valor seleccionado**')
     st.write(tickerDf2)
@@ -107,22 +107,22 @@ elif analysis == "CryptoUSD":
     # Date picker
     st.header('Selecciona valor')
 
-    ticker_list2=pd.read_csv('crypto.txt')
-    tickerSymbol2=st.selectbox('Denominación de la cryptomoneda', ticker_list2)
-    tickerData2=yf.Ticker(tickerSymbol2)
+    ticker_list3=pd.read_csv('crypto.txt')
+    tickerSymbol32=st.selectbox('Denominación de la cryptomoneda', ticker_list3)
+    tickerData3=yf.Ticker(tickerSymbol3)
     #La fecha a seleccionar podemos ponerla en body o en la sidebar añadiendo stt.sidebar
-    start_date2=st.date_input("Fecha inicial", datetime.date(2021, 1, 1))
-    end_date2=st.date_input("Fecha final", datetime.date(2021, 3, 15))
-    tickerDf2 = tickerData2.history(period='1d', start=start_date2, end=end_date2)
-    string_logo2 = '<img src=%s>' % tickerData2.info['logo_url']
-    st.markdown(string_logo2, unsafe_allow_html=True)
+    start_date3=st.date_input("Fecha inicial", datetime3.date(2021, 1, 1))
+    end_date3=st.date_input("Fecha final", datetime3.date(2021, 3, 15))
+    tickerDf3 = tickerData3.history(period='1d', start=start_date3, end=end_date3)
+    string_logo3 = '<img src=%s>' % tickerData3.info['logo_url']
+    st.markdown(string_logo3, unsafe_allow_html=True)
 
     st.header('**Representación sencilla del valor seleccionado**')
-    tickercopyDf=tickerDf
-    tickercopyDf=tickercopyDf.drop(['Volume', 'Dividends','Stock Splits'], axis=1)
-    st.line_chart(tickercopyDf,use_container_width=True)
+    tickercopyDf3=tickerDf3
+    tickercopyDf3=tickercopyDf3.drop(['Volume', 'Dividends','Stock Splits'], axis=1)
+    st.line_chart(tickercopyDf3,use_container_width=True)
 
     st.header('**Tabla/DF del valor seleccionado**')
-    st.write(tickerDf2)
+    st.write(tickerDf3)
 
 
